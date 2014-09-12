4c1a68457a1e62c5ff1f83a497affddb31d2fea1
Lawrence Huang <mr.godfather@gmail.com>
2014-09-12 15:18:13 +0800

    change coll.clj

--- src/clj_tour/coll.clj
+++ src/clj_tour/coll.clj
@@ -1,7 +1,7 @@
 (ns clj-tour.coll
   (:require [clojure.pprint]
             [clojure.string :as str])
-  (:use clojure.repl))
+  (:use [clojure.repl]))
 
 (defn swap-pair
   [coll]
@@ -48,3 +48,61 @@
   (lazy-seq
    (println "Realizing lazy seq.....")
    (cons (rand-int limit) (random-ints limit))))
+
+(defn magnitude
+  [x]
+  (-> x
+      Math/log10
+      Math/floor))
+
+(defn compare-magnitude
+  [a b]
+  (- (magnitude a) (magnitude b)))
+
+
+;; Clojure Programming page 118-123, READ IT!
+(def playlist
+  [{:title "Elephant", :artist "The White Stripes", :year 2003}
+   {:title "Helioself", :artist "Papas Fritas", :year 1997}
+   {:title "Stories from the City, Stories from the Sea",
+    :artist "PJ Harvey", :year 2000}
+   {:title "Buildings and Grounds", :artist "Papas Fritas", :year 2000}
+   {:title "Zen Rodeo", :artist "Mardi Gras BB", :year 2002}])
+
+(defn summarize
+  [{:keys [title artist year]}]
+  (str title " // " artist " // " year))
+
+;; (map summarize playlist)
+;; (group-by :artist playlist)
+;; (group-by (juxt :artist :year) playlist)
+;; (group-by #(odd? %) (range 10))
+;; (group-by #(rem % 3) (range 10))
+(into {} (for [[k v] (group-by :artist playlist)]
+           [k (count v)]))
+
+(def orders
+  [{:product "Clock", :customer "Wile Coyote", :qty 6, :total 300}
+   {:product "Dynamite", :customer "Wile Coyote", :qty 20, :total 5000}
+   {:product "Shotgun", :customer "Elmer Fudd", :qty 2, :total 800}
+   {:product "Shells", :customer "Elmer Fudd", :qty 4, :total 100}
+   {:product "Hole", :customer "Wile Coyote", :qty 1, :total 1000}
+   {:product "Hole", :customer "Wile Coyote", :qty 12, :total 11000}
+   {:product "Anvil", :customer "Elmer Fudd", :qty 2, :total 300}
+   {:product "Anvil", :customer "Wile Coyote", :qty 6, :total 900}])
+
+(into {} (for [[k v] (group-by :product orders)]
+           [k (apply + (map :total v))]))
+
+(defn reduce-by
+  [key-fn f init coll]
+  (reduce (fn
+            [summary x]
+            (let [k (key-fn x)]
+              (assoc summary k (f (summary k init) x))))
+          {}
+          coll))
+
+(reduce-by :customer #(+ %1 (:total %2)) 0 orders)
+(reduce-by :product #(conj %1 (:customer %2)) #{} orders)
+(reduce-by (juxt :customer :product) #(+ %1 (:total %2)) 0 orders)
